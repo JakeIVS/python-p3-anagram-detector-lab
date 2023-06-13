8092fd859723280a56bbe201b638ab3f366a36d2
Jacob Ives <jacob.w.ives@gmail.com>
2023-06-13 14:24:22 -0600

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram():
+    pass
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        anagrams = []
+        for word in word_list:
+            if sorted([letter for letter in word]) == sorted([letter for letter in self.word]):
+                anagrams.append(word)
+
+        return anagrams
